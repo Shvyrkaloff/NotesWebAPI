0c63172a09b91ff444ee83abc59958a2f5dfbd9f
Shvyrkaloff <shvyrkalovm@mail.ru>
2023-03-09 13:04:29 +0300

    DoneMediatR

---
+++ NotesApplication/Notes/Queries/GetNotesList/GetListNoteQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace NotesApplication.Notes.Queries.GetNotesList
+{
+    public class GetListNoteQuery : IRequest<NoteListVm>
+    {
+        public Guid UserId { get; set; }
+    }
+}
