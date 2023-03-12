ffa553ad231bd37e1045be2d428fc092df9841c7
Shvyrkaloff <shvyrkalovm@mail.ru>
2023-03-12 19:54:55 +0300

    DeleteFix

--- NotesWebApi/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
+++ NotesWebApi/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
@@ -1,17 +1,23 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using NotesApplication.Common.Exception;
 using NotesPresistence;
-
 namespace NotesWebApi.Notes.Commands.DeleteCommand
 {
+
+    public class DeleteNoteCommand : IRequest<Unit>
+    {
+        public string UserId { get; set; }
+        public string Id { get; set; }
+    }
     public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommand, Unit>
     {
         private readonly INotesDbContext _dbContext;
         public DeleteNoteCommandHandler(INotesDbContext dbContext) => _dbContext = dbContext;
         public async Task<Unit> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
         {
-            var entity = await _dbContext.Notes.FindAsync(new object[] { request.Id }, cancellationToken);
-            if (entity == null || entity.UserId != request.UserId.ToString())
+            var entity = await _dbContext.Notes.FirstOrDefaultAsync(note => note.Id == request.Id.ToString(), cancellationToken);
+            if (entity == null)
             {
                 throw new NotFoundException(nameof(Note), request.Id);
             }
